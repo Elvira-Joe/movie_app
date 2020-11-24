931fc1b2a3355c45850da17fbc0d495799489077
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-11-24 15:45:39 -0600

    refactoring function to display cards

--- js/movies.js
+++ js/movies.js
@@ -2,30 +2,24 @@
 
 const apiURL = "https://wind-miniature-gauge.glitch.me/movies";
 
+
+//getMovies Function to fetch
 const getMovies = () => fetch(apiURL)
     .then(res => res.json())
     .catch(console.error)
 
-function buildCards(movies){
-    let html = `<div>`
-    for(let movie of movies){
-        console.log(movie.title);
-        console.log(movie.rating);
-        console.log(movie.id);
 
-        html += '<div class="card">\n' +
-            '  <div class="card-header">\n' +
-            '    Quote\n' +
-            '  </div>\n' +
-            '  <div class="card-body">\n' +
-            '    <blockquote class="blockquote mb-0">\n' +
-            '      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>\n' +
-            '      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>\n' +
-            '    </blockquote>\n' +
-            '  </div>\n' +
-            '</div>'
-
-    }
-    html += `</div>`
-    document.write(html);
+//Building Cards Function
+function buildCards(movie){
+    let html = '';
+    html += `<div class="card">
+     <div class="card-header">${movie.title}</div>
+     <div class="card-body">
+     <blockquote class="blockquote mb-0">
+     <p>${movie.rating}</p>
+     <footer class="blockquote-footer">${movie.id} <cite title="Source Title">Source Title</cite></footer>
+     </blockquote>
+     </div>
+     </div>`;
+    return html;
 }
