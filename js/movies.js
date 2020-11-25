6c2910d63752a39ec8a5fd53c85750f846d3d39e
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2020-11-25 09:52:18 -0600

    changed bootstrap cards and formatted page

--- js/movies.js
+++ js/movies.js
@@ -12,14 +12,34 @@ const getMovies = () => fetch(apiURL)
 //Building Cards Function
 function buildCards(movie){
     let html = '';
-    html += `<div class="card">
-     <div class="card-header">${movie.title}</div>
-     <div class="card-body">
-     <blockquote class="blockquote mb-0">
-     <p>${movie.rating}</p>
-     <footer class="blockquote-footer">${movie.id} <cite title="Source Title">Source Title</cite></footer>
-     </blockquote>
-     </div>
-     </div>`;
+
+    html += `<div class="card mb-3" style="max-width: 540px;">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="${movie.poster}" class="card-img" alt="${movie.title}">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title movie-title">${movie.title}</h5>
+            <p class="card-text movie-plot">${movie.plot}</p>
+            <p class="card-text movie-id">${movie.id}</p>
+            <p class="card-text movie-rating"><small class="text-muted"><strong>${movie.rating}</strong></small></p>
+          </div>
+        </div>
+      </div>
+    </div>`
+
+    // html += `<div class="card">
+    //  <img src="${movie.poster}" class="movie-posters">
+    //  <div class="card-body">
+    //  <h5 class="card-title movie-title">${movie.title}</h5>
+    //  <p class="card-text">${movie.plot}</p>
+    //  <p class="movie-id">${movie.id}</p>
+    //  </div>
+    //  <div class="card-footer">
+    //  <small class="text-muted"><strong>Rating: ${movie.rating} Stars</strong></small>
+    //  </div>
+    //  </div>`;
     return html;
 }
+
